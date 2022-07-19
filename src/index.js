abb9e6cdbd9397e627e4052c1e0d091c9f5a3d90
Ttatyanakr <Ttatyanakr@yandex.ru>
2022-07-19 07:42:54 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+module.exports = function towelSort(matrix) {
+  
+  let emptyArr = [];
+  function isEmpty(obj) {
+    for (let key in obj) {
+      return false;
+    }
+    return true;
+  }
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+  if (isEmpty(matrix)) {
+    return emptyArr;
+  }  else {
+    for (let i = 0; i < matrix.length; i++) {
+      (i % 2 === 0) ? matrix[i].sort((a, b) => a - b) : matrix[i].sort((a, b) => b - a);
+    }
+  
+    return matrix.flat();
+  }
 }
